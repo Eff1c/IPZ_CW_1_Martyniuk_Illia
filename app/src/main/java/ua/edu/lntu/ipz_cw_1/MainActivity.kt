525fd29a40081a0665c666653c980986f3d504ec
eff1c <illia.martyniuk@busmarket.ua>
2024-03-11 14:26:34 +0200

    add image view

--- app/src/main/java/ua/edu/lntu/ipz_cw_1/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/ipz_cw_1/MainActivity.kt
@@ -3,12 +3,15 @@ package ua.edu.lntu.ipz_cw_1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import ua.edu.lntu.ipz_cw_1.ui.theme.IPZ_CW_1_Martyniuk_IlliaTheme
 
@@ -22,25 +25,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Column {
+                        Image(painter = painterResource(id = R.drawable.android_logo), contentDescription = "")
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    IPZ_CW_1_Martyniuk_IlliaTheme {
-        Greeting("Android")
     }
 }
